aee84ab23175cfcbf46ddd034ea85c55e9849b31
Amr Abd El Fattah <amrabdelfattah@hotmail.fr>
2021-03-13 02:34:38 +0100

    Une rosace avec Canvas & JS

---
+++ app.js
@@ -0,0 +1,33 @@
+let canvas  = document.getElementById("canvas"); 
+let ctx     = canvas.getContext ("2d"); 
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; 
+
+var x       = 0; 
+var y       = 0; 
+var tour    = 0; 
+ctx.translate(innerWidth / 2, innerHeight / 2); 
+
+function dessin (){
+
+    requestAnimationFrame(dessin)
+
+    ctx.beginPath(); 
+    ctx.strokeStyle = "#EDF5E1"; 
+    ctx.arc (x,y,100,0,2* Math.PI); 
+    ctx.stroke(); 
+
+    ctx.rotate(20 * Math.PI / 180); 
+    tour  = tour + 20; 
+    if (tour > 7200) {
+        return; 
+    }
+ 
+    if (tour % 360 === 0) {
+        x += 10; 
+        y += 10; 
+    }
+
+}
+dessin(); 
